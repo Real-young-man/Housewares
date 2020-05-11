eb6806767d69cab40db2b20996146855e5210cdf
ssj <135366280@qq.com>
2020-05-11 18:48:35 +0800

    增加部分功能

--- app/src/main/java/com/ssj/housewares/ui/home/HomeFragment.java
+++ app/src/main/java/com/ssj/housewares/ui/home/HomeFragment.java
@@ -1,8 +1,6 @@
 package com.ssj.housewares.ui.home;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
